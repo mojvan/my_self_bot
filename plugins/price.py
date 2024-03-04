bfb540367d8942be3dbfc109a5ac9535240af63f
mojvan <mojvan.mojvan@gmail.com>
2024-03-04 19:58:43 +0330

    join group and send message

--- plugins/price.py
+++ plugins/price.py
@@ -5,5 +5,12 @@ cg = CoinGeckoAPI()
 
 @Client.on_message(filters.regex("قیمت"))
 async def my_handler(client: Client, message:Message):
-    price =  cg.get_price(ids='bitcoin', vs_currencies='usd')
-    await message.reply_text(f"{price["bitcoin"]["usd"]}")
+    # price =  cg.get_price(ids='bitcoin', vs_currencies='usd')
+    # await message.reply_text(f"{price["bitcoin"]["usd"]}")
+    #
+    #
+    # await client.join_chat("hamamsssss")
+    chat = await client.get_chat("hamamsssss")
+    print(chat.id)
+    await client.send_message(chat_id=int(chat.id),text="hello")
+    # print(chat.id)
